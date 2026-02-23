dd91e84f6189e46c5f2f4b76fd68979e7b4cbec2
Vinicius Lenovo <contato@webcube.com.br>
2026-02-23 11:10:06 -0300

    Ajuste

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $pendingOrdersCount = 0;
+
+        if (auth()->check() && auth()->user()->isAdmin()) {
+            $pendingOrdersCount = Order::query()
+                ->where('review_status', 'PENDENTE')
+                ->whereNull('admin_viewed_at')
+                ->count();
+        }
+
+        return view('dashboard', compact('pendingOrdersCount'));
+    }
+}
