188fc9a5e075dc3339b97cd3292ed4fe8f786808
Vinicius Lenovo <contato@webcube.com.br>
2026-04-01 19:51:31 -0300

    Migration para aceitar outros valores de event_name no financial_logs

---
+++ database/migrations/2026_04_01_225035_alter_event_name_on_financial_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        DB::statement("
+            ALTER TABLE financial_logs
+            MODIFY event_name VARCHAR(100) NOT NULL
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE financial_logs
+            MODIFY event_name ENUM(
+                'CREATED',
+                'UPDATED',
+                'STATUS_CHANGED',
+                'ASAAS_CREATED',
+                'ASAAS_UPDATED',
+                'ASAAS_STATUS_CHANGED',
+                'ASAAS_WEBHOOK_RECEIVED'
+            ) NOT NULL
+        ");
+    }
+};
